255181e52e15e84769b04577c4d6a9d0f0ed3f98
James Hunt <james@niftylogic.com>
2020-09-18 14:20:26 -0400

    Enable bosh create-release debugging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 set -eu
 
-echo "==================="
-env
-echo "==================="
-set
-echo "==================="
-
 VERSION=$INPUT_VERSION
 TARBALL="v$VERSION.tgz"
 
+set -x
 bosh create-release --final --version "$VERSION" --tarball "$TARBALL"
 echo "::set-output name=tarball::$PWD/$TARBALL"
