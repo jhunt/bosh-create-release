6a67c8ffee3114281e89cafcd6dd2fabda8c7873
James Hunt <james@niftylogic.com>
2020-09-18 19:09:21 -0400

    Support Blobstore Access via s3

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 set -eu
-bosh sync-blobs
-bosh create-release --final --version "$INPUT_VERSION" --tarball "$INPUT_TARBALL"
-chmod 0644 $INPUT_TARBALL
+cat > config/private.yml <<EOF
+---
+blobstore:
+  provider: s3
+  options:
+    access_key_id:     $INPUT_S3_ACCESS_KEY_ID
+    secret_access_key: $INPUT_S3_SECRET_ACCESS_KEY
+EOF
+bosh create-release --version "$INPUT_VERSION" --tarball "$INPUT_TARBALL" --final
+chmod 0644 "$INPUT_TARBALL"
