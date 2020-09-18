a178bbc160295c673a0feee2dc77b77a106270c3
James Hunt <james@niftylogic.com>
2020-09-18 14:28:27 -0400

    Explicitly set tarball path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 set -eu
-
-VERSION=$INPUT_VERSION
-TARBALL="v$VERSION.tgz"
-
-set -x
-bosh create-release --final --version "$VERSION" --tarball "$TARBALL"
-echo "::set-output name=tarball::$PWD/$TARBALL"
+bosh create-release --final --version "$INPUT_VERSION" --tarball "$INPUT_TARBALL"
